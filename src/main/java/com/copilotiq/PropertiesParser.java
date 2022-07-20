21ffa6737877cdf5a3e31791e16d3ccc5bf05328
Ezequiel Coronel <ezequiel.coronel@wenance.com>
2022-07-19 22:26:20 -0300

    Added Unit tests and completed the code logic.

--- src/main/java/com/copilotiq/PropertiesParser.java
+++ src/main/java/com/copilotiq/PropertiesParser.java
@@ -7,13 +7,13 @@ import java.util.regex.Pattern;
 
 /**
  * <h3>PropertiesParser</h3>
- *
+ * <p>
  * Resolves key substitution on a properties set; in other words, given:
  * <pre>FOO = foo
  * BAR = bar
  * baz = ${FOO} is ${BAR}
  * </pre>
- *
+ * </p>
  * the following code will satisfy the assertion:
  * <pre> Properties props = Properties.load('my.properties');
  *  PropertiesParser parser = new PropertiesParser(props);
@@ -24,10 +24,10 @@ import java.util.regex.Pattern;
 public class PropertiesParser {
 
 
-    /** A RegEx pattern for a key subst: ${key} */
-
-
-    private final String PATTERN = "${.*}";
+    /**
+     * A RegEx pattern for a key subst: ${key}. Using <code>(.*)</code> allows to extract the value directly
+     */
+    private final String PATTERN = "\\$\\{([^$]+)}";
     private final Properties props;
 
 
@@ -39,8 +39,10 @@ public class PropertiesParser {
     public void parse() {
         for (String key : props.stringPropertyNames()) {
             String value = props.getProperty(key);
-            if (hasSubstKey(value))
+            if (hasSubstKey(value)) {
                 value = applyPattern(value);
+                props.setProperty(key, value);
+            }
         }
     }
 
@@ -48,32 +50,46 @@ public class PropertiesParser {
     /**
      * Executes string substitution for the {@code value}
      *
-     * @param value
+     * @param value string to evaluate
      * @return the fully substituted value, if possible
      */
     protected String applyPattern(String value) {
         Pattern keyPattern = Pattern.compile(PATTERN);
         Matcher m = keyPattern.matcher(value);
         StringBuilder sb = new StringBuilder();
-        if (m.find()) {
-            int curPos = 0;
-            sb.append(value.substring(curPos, m.start())).append(extractKeyValue(m.group()));
-            sb.append(value.substring(m.end()));
+        int curPos = 0;
+        while (m.find()) {
+            sb.append(value, curPos, m.start()).append(extractKeyValue(m.group(1)));
+            curPos = m.end();
         }
+        sb.append(value.substring(curPos));
         return sb.toString();
     }
 
-
-    /** Replaces the subst key with its value, if available in {@code props} */
-    // TODO: improve javadoc
-    private String extractKeyValue(String key) {
-        //TODO("Not yet implemented");
-        return null;
+    /**
+     * Replaces the subst key with its value, if available in {@code props}
+     *
+     * @param key searched subst key
+     * @return the value if defined or empty string
+     */
+    final protected String extractKeyValue(String key) {
+        String value = "";
+        if (props.containsKey(key)) {
+            value = props.getProperty(key);
+        }
+        return value;
     }
 
 
-    private boolean hasSubstKey(String value) {
-        //TODO("Not yet implemented");
-        return false;
+    /**
+     * Confirms the value contains a reference to a substitution key
+     *
+     * @param value validated value
+     * @return whether the value contains a reference in the defined format <code>${key}</code>
+     */
+    final protected boolean hasSubstKey(String value) {
+        Pattern keyPattern = Pattern.compile(PATTERN);
+        Matcher m = keyPattern.matcher(value);
+        return m.find();
     }
 }
---
+++ src/test/java/com/copilotiq/PropertiesParserTest.java
@@ -0,0 +1,55 @@
+package com.copilotiq;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import java.io.IOException;
+import java.util.Properties;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+class PropertiesParserTest {
+
+    @Test
+    void parse() {
+        Properties props = new Properties();
+        PropertiesParser parser = new PropertiesParser(props);
+        props.setProperty("FOO", "foo");
+        props.setProperty("BAR", "bar");
+        props.setProperty("baz", "${FOO} is ${BAR}");
+        parser.parse();
+        assertEquals("foo", props.getProperty("FOO"));
+        assertEquals("bar", props.getProperty("BAR"));
+        assertEquals("foo is bar", props.getProperty("baz"));
+    }
+
+    @Test
+    void applyPattern() {
+        Properties props = new Properties();
+        PropertiesParser parser = new PropertiesParser(props);
+        props.setProperty("FOO", "foo");
+        props.setProperty("BAR", "bar");
+        props.setProperty("baz", "${FOO} is ${BAR}");
+        assertEquals("plain text", parser.applyPattern("plain text"));
+        assertEquals("foo is bar", parser.applyPattern("${FOO} is ${BAR}"));
+    }
+
+    @Test
+    void extractKeyValue() {
+        Properties props = new Properties();
+        props.setProperty("FOO", "foo");
+        PropertiesParser parser = new PropertiesParser(props);
+        assertEquals("foo", parser.extractKeyValue("FOO"));
+        assertEquals("", parser.extractKeyValue("UNK"));
+    }
+
+    @Test
+    void hasSubstKeyPresent() {
+        Properties props = new Properties();
+        PropertiesParser parser = new PropertiesParser(props);
+        assertTrue(parser.hasSubstKey("References to ${foo}"));
+        assertFalse(parser.hasSubstKey("A plain value without substitutions"));
+    }
+}
